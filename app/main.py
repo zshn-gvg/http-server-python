9020067c4a5bad9c9767dc9dc584a8dbba5ba4e9
zshn <50868318+zshn-gvg@users.noreply.github.com>
2023-10-22 15:30:32 +0200

    stage 4

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 # Uncomment this to pass the first stage
 import socket
-
+import re
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
@@ -9,13 +9,14 @@ def main():
     connection, address = server_socket.accept()
     print(f"Accepted connection from {address}")
 
-    data = connection.recv(1024).decode("utf-8")
+    data = connection.recv(1024).decode()
     print(f"Received data:\n{data}")
 
     path = data.split()[1]
-
-    if path == "/":
-       response = "HTTP/1.1 200 OK\r\n\r\n"
+    match = re. match = re.match(r"/echo/(\w+)", path)
+    if match:
+        rnd_string = match.group(1)
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(rnd_string)}\r\n\r\n{rnd_string}"
     else:
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
     
