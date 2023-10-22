d7162732676c0dddded4fdbf9d4aa7b5131ca552
zshn <50868318+zshn-gvg@users.noreply.github.com>
2023-10-22 16:26:39 +0200

    stg6

--- app/main.py
+++ app/main.py
@@ -1,20 +1,10 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
 
-def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    print("Server listening on localhost:4421")
-
-    connection, address = server_socket.accept()
-    print(f"Accepted connection from {address}")
-
-    req_data = connection.recv(1024).decode()
-    print(f"Received data:\n{req_data}")
-
-    data = req_data.split()
-
-    if len(data) >0 and data[0] == "GET":
-        path = data[1]
+def handle_requests(connection):
+        req_data = connection.recv(1024).decode()
+        data = req_data.split()
         if len(data) > 1 and data[0] == "GET":
             path = data[1]
             if path == "/":
@@ -27,9 +17,27 @@ def main():
                 response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(agent_value)}\r\n\r\n{agent_value}"
             else:
                 response = "HTTP/1.1 404 Not Found\r\n\r\n"
-    
-    connection.sendall(response.encode())
-    connection.close()
+        connection.sendall(response.encode())
+        connection.close()
+
+
+def main():
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket.listen(10)
+    print("Server listening on localhost:4421")
+
+
+
+    req_data = connection.recv(1024).decode()
+    print(f"Received data:\n{req_data}")
+
+    while True:
+        connection, address = server_socket.accept()
+        print(f"Accepted connection from {address}")
+
+        thread = threading.Thread(target=handle_requests, args=(connection))
+        thread.start()
+
 
 if __name__ == "__main__":
     main()
