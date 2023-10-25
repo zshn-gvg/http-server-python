29546650c89081f6d9570053447158ad9d94aa0e
zshn <50868318+zshn-gvg@users.noreply.github.com>
2023-10-25 23:10:49 +0200

    clean up of code stage8

--- app/main.py
+++ app/main.py
@@ -1,48 +1,61 @@
 import socket
 import threading
 
-def handle_requests(connection):
-        req_data = connection.recv(1024).decode().split("\r\n")
-        data = req_data[0].split()
-        if (
-            len(data) > 2 
-            and data[0] == "GET" 
-            and data[1] == "/user-agent"
-        ):
-            agent_value = None
-            for header in req_data[1:]:
-                if header.startswith("User-Agent: "):
-                    agent_value = header[len("User-Agent: ") :]
-                    break
-            if agent_value:
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(agent_value)}\r\n\r\n{agent_value}"
-            else:
-                response = "HTTP/1.1 400 Bad Request\r\n\r\nNo User-Agent header found"
-        else:
-            path = data[1]
-            if path == "/":
-                response = "HTTP/1.1 200 OK\r\n\r\n"
-            elif path.startswith("/echo/"):
-                rndm_string = path[6:]
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(rndm_string)}\r\n\r\n{rndm_string}"
-            else:
-                response = "HTTP/1.1 404 Not Found\r\n\r\nNot Found"
-        connection.sendall(response.encode())
-        connection.close()
+RESPONSE_200 = "HTTP/1.1 200 OK\r\n\r\n"
+RESPONSE_404 = "HTTP/1.1 404 Not Found\r\n\r\n"
 
+def extract_path(request):
+    request_by_line = request.split("\r\n")
+    HTTPmethod, path, discard = request_by_line[0].split()
+    return path
+
+def extract_user_agent(request):
+        request_by_line = request.split("\r\n")
+        for line in request_by_line[1:]:
+            if line.startswith("User-Agent: "):
+                return line.split(":", 1)[1].strip()
+            
+def handle_user_agent(req_data):
+    user_agent = extract_user_agent(req_data)
+    response_head = f"""{RESPONSE_200}Content-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n"""
+    response = response_head + user_agent
+    return response
+
+def handle_echo(path):
+    to_echo_string = path[len("/echo")]
+    response_head = f"""{RESPONSE_200}Content-Type: text/plain\r\nContent-Length: {len(response_body)}\r\n\r\n"""
+    response = response_head + to_echo_string
+    return response
+
+def handle_requests(connection):
+        req_data = connection.recv(1024).decode()
+        path = extract_path(req_data)
+        if path == "/":
+             connection.sendall(RESPONSE_200.encode())
+        elif path.startswith("/files"):
+            response = RESPONSE_200 + "todo"
+            connection.sendall(response.encode())
+        elif path.startswith("user-agent"):
+             response = handle_user_agent(req_data)
+             connection.sendall(response.encode())
+        elif path.startswith("/echo/"):
+             response = handle_echo(req_data)
+             connection.sendall(response.encode())
+        else:
+             connection.sendall(RESPONSE_404.encode())
+        connection.close()
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.listen(5)
+    # server_socket.listen(5)
     print("Server listening on localhost:4421")
     while True:
         connection, address = server_socket.accept()
-        # print(f"Accepted connection from {address}")
-        # req_data = connection.recv(1024).decode()
-        # print(f"Received data:\n{req_data}")
+        print(f"Accepted connection from {address}")
+        req_data = connection.recv(1024).decode()
+        print(f"Received data:\n{req_data}")
         thread = threading.Thread(target=handle_requests, args=(connection,))
         thread.start()
-
 
 if __name__ == "__main__":
     main()
