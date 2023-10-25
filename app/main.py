0a66e228eee3c28f513cdbfc6c26c1802d8a5495
zshn <50868318+zshn-gvg@users.noreply.github.com>
2023-10-26 00:13:59 +0200

    post files try1 stage8

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 RESPONSE_200 = "HTTP/1.1 200 OK\r\n\r\n"
+RESPONSE_201 = "HTTP/1.1 201 Resource Created\r\n\r\n"
 RESPONSE_404 = "HTTP/1.1 404 Not Found\r\n\r\n"
 
 
@@ -11,6 +12,19 @@ def extract_path(request):
     request_by_line = request.split("\r\n")
     HTTPmethod, path, _ = request_by_line[0].split( )
     return path
+
+def extract_method(request):
+    request_by_line = request.split("\r\n")
+    HTTPmethod, path, _ = request_by_line[0].split( )
+    return HTTPmethod
+
+def handle_post_file(request, path):
+    filename = path.split("/")[2]
+    body = request.split("\r\n\r\n")[1]
+    dir = sys.argv[-1]
+    fd = open(dir + filename, mode="wb")
+    fd.write(body.encode())
+    return RESPONSE_201 
 
 
 def extract_user_agent(request):
@@ -32,6 +46,8 @@ def handle_echo(path):
     response_head = f"""HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(to_echo_string)}\r\n\r\n"""
     response = response_head + to_echo_string
     return response
+
+
 def handle_files(path):
         filename = path.split("/")[2]
         dir = sys.argv[-1]
@@ -61,19 +77,25 @@ def main():
 def handle_requests(connection):
     req_data = connection.recv(1024).decode("utf-8")
     path = extract_path(req_data)
-    if path == "/":
-        connection.sendall(RESPONSE_200.encode())
-    elif path.startswith("/files"):
-        response = handle_files(path)
-        connection.sendall(response.encode())
-    elif path.startswith("/user-agent"):
-        response = handle_user_agent(req_data)
-        connection.sendall(response.encode())
-    elif path.startswith("/echo/"):
-        response = handle_echo(path)
-        connection.sendall(response.encode())
-    else:
-        connection.sendall(RESPONSE_404.encode())
+    method = extract_method(req_data)
+    if method == "GET":
+        if path == "/":
+            connection.sendall(RESPONSE_200.encode())
+        elif path.startswith("/files"):
+            response = handle_files(path)
+            connection.sendall(response.encode())
+        elif path.startswith("/user-agent"):
+            response = handle_user_agent(req_data)
+            connection.sendall(response.encode())
+        elif path.startswith("/echo/"):
+            response = handle_echo(path)
+            connection.sendall(response.encode())
+        else:
+            connection.sendall(RESPONSE_404.encode())
+    elif method == "POST":
+        if path.startswith("/files"):
+            response = handle_post_file(req_data, path)
+            connection.sendall(response.encode())
     connection.close()
 
 
